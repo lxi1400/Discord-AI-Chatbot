6a5562f496c12b543a3081be1c5f9ae6fc7e071a
lxi <75556084+lxi1400@users.noreply.github.com>
2021-02-26 18:02:41 -0600

    Made it so it actually understands you
    Also cleaned up code

--- bot.py
+++ bot.py
@@ -6,7 +6,8 @@ class utils:
         os.system('cls')
     def rename(title):
         os.system("title {}".format(title))
-    def request_handler(message, apikey, bid, id):
+class request:
+    def get(message, apikey, bid, id):
         r = requests.get(url=f"http://api.brainshop.ai/get?bid={bid}&key={apikey}&uid={id}&msg={message}")
         return r.json()['cnt']
 
@@ -25,7 +26,7 @@ client = discord.Client(intents=intents)
 async def on_ready():
     utils.clear()
     utils.rename("Chat Bot [Logged into {}#{}]".format(str(client.user.name), str(client.user.discriminator)))
-    print(f"[STATUS] Logged into => {client.user.name}#{client.user.discriminator}")
+    print(f"Logged into => {client.user.name}#{client.user.discriminator}")
 
 
 # -------- On Message -------- #
@@ -34,10 +35,10 @@ async def on_ready():
 async def on_message(message):
     if message.author.id == client.user.id:
         return
-    print("[STATUS] Message revived: {} from author: {}".format(message.content, message.author.name))
-    msg = utils.request_handler(message, apikey, bid, message.author.id)
+    print("{} > {}".format(message.content, message.author.name))
+    msg = request.get(message.content, apikey, bid, message.author.id)
     await message.channel.send(msg)
-    print("[STATUS] Responded to {} from author {} with {}".format(message.content, message.author.name, msg))
+    print("Bot [To {}] > {}".format(message.author.name, msg))
 
 
 # -------- Run Bot -------- #
